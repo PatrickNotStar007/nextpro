2b7532e3f4918b0d7dda22651d8855d75f740db7
Patrick <h.ilya2003@mail.ru>
2026-03-10 17:23:04 +0300

    implement loading skeleton

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -1,13 +1,13 @@
 import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
 
-const BlogPage = async () => {
-  const data = await fetchQuery(api.post.getPosts);
-
+const BlogPage = () => {
   return (
     <div className="py-12">
       <div className="text-center pb-12">
@@ -19,39 +19,66 @@ const BlogPage = async () => {
         </p>
       </div>
 
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {data?.map((post) => (
-          <Card key={post._id} className="pt-0">
-            <div className="relative h-48 w-full overflow-hidden">
-              <Image
-                src="https://static.vecteezy.com/system/resources/previews/007/233/800/non_2x/sea-beach-and-rocky-mountains-in-tropical-evening-photo.jpg"
-                alt="image"
-                fill
-                className="rounded-t-lg"
-              />
-            </div>
-
-            <CardContent>
-              <Link href={`/blog/${post._id}`}>
-                <h1 className="text-2xl font-bold hover:text-primary">
-                  {post.title}
-                </h1>
-              </Link>
-              <p className="text-muted-foreground line-clamp-3">{post.body}</p>
-            </CardContent>
-            <CardFooter>
-              <Link
-                className={buttonVariants({ className: "w-full" })}
-                href={`/blog/${post._id}`}
-              >
-                Read more
-              </Link>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+      <Suspense fallback={<SekeletonLoadingUi />}>
+        <LoadBlogList />
+      </Suspense>
     </div>
   );
 };
 
 export default BlogPage;
+
+const LoadBlogList = async () => {
+  const data = await fetchQuery(api.post.getPosts);
+
+  return (
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      {data?.map((post) => (
+        <Card key={post._id} className="pt-0">
+          <div className="relative h-48 w-full overflow-hidden">
+            <Image
+              src="https://static.vecteezy.com/system/resources/previews/007/233/800/non_2x/sea-beach-and-rocky-mountains-in-tropical-evening-photo.jpg"
+              alt="image"
+              fill
+              className="rounded-t-lg"
+            />
+          </div>
+
+          <CardContent>
+            <Link href={`/blog/${post._id}`}>
+              <h1 className="text-2xl font-bold hover:text-primary">
+                {post.title}
+              </h1>
+            </Link>
+            <p className="text-muted-foreground line-clamp-3">{post.body}</p>
+          </CardContent>
+          <CardFooter>
+            <Link
+              className={buttonVariants({ className: "w-full" })}
+              href={`/blog/${post._id}`}
+            >
+              Read more
+            </Link>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+const SekeletonLoadingUi = () => {
+  return (
+    <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-3">
+      {[...Array(3)].map((_, i) => (
+        <div className="flex flex-col space-y-3" key={i}>
+          <Skeleton className="h-48 w-full rounded-xl" />
+          <div className="space-y-2 flex flex-col">
+            <Skeleton className="h-6 w-3/4" />
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-6 w-2/3" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
