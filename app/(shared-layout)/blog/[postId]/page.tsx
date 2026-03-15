2a5d0f808980e5307f6065234bc0b83d7132fc3d
Patrick <h.ilya2003@mail.ru>
2026-03-15 12:14:11 +0300

    implement multi layer auth check

--- app/(shared-layout)/blog/[postId]/page.tsx
+++ app/(shared-layout)/blog/[postId]/page.tsx
@@ -10,6 +10,7 @@ import { ArrowLeft } from "lucide-react";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 interface PostIdRouteProps {
   params: Promise<{ postId: Id<"posts"> }>;
@@ -48,6 +49,10 @@ const PostIdRoute = async ({ params }: PostIdRouteProps) => {
     }),
     await fetchQuery(api.presence.getUserId, {}, { token }),
   ]);
+
+  if (!userId) {
+    return redirect("/auth/login");
+  }
 
   if (!post) {
     return (
